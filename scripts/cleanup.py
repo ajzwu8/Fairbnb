db39c8cf644f542e4fb8ea7c1aa15f127d3d5846
ajzwu8 <ajw238@cornell.edu>
2016-10-28 16:31:14 -0400

    one hot encoding

--- scripts/cleanup.py
+++ scripts/cleanup.py
@@ -1,10 +1,10 @@
 import pandas as pd
 import numpy as np
+from datetime import datetime
 from ast import literal_eval
 
 CSV_FILE = "../listings.csv"    # csv file of data
 ROWS_FILE = "rows_to_keep.txt"  # text containing rows to keep
-
 
 def convert_to_feature(data):
 	""" Applies this function to every cell in the csv. Goal is
@@ -18,29 +18,54 @@ def convert_to_feature(data):
 	if pd.isnull(data):
 		return 0
 
+	# special case for lists
+	try:
+		eval_data = literal_eval(data)
+		if type(eval_data) == list:
+			return len(eval_data)
+	except:
+		pass
+
+	# special case for dates
+	try:
+		date_data = datetime.strptime(data, '%Y-%m-%d')
+		now = datetime.now()
+		return (now.year - date_data.year) * 12 + (now.month - date_data.month)
+	except:
+		pass
+
 	# for strings, try and clean up into an int
 	if type(data) == str:
-		# special case for lists
-		try:
-			eval_data = literal_eval(data)
-			if type(eval_data) == list:
-				return len(eval_data)
-		except:
-			# turn booleans into bits
-			if data == 't':
-				return 1
-			elif data == 'f':
-				return 0
-			# turn prices into numbers
-			elif '$' in data:
-				return float(data[1:].replace(',',''))
-			# percents become decimals
-			elif '%' in data:
-				return float(data.replace('%',''))
-
+		# turn booleans into bits
+		if data == 't':
+			return 1
+		elif data == 'f':
+			return 0
+		# turn prices into numbers
+		elif '$' in data:
+			return float(data[1:].replace(',',''))
+		# percents become decimals
+		elif '%' in data:
+			return float(data.replace('%',''))/100.0
+		# amenities are a set in this {format}, # amenities = # commas + 1
+		elif '{' in data and '}' in data:
+			return data.count(',') + 1
+		
 	# default, return itself
 	return data
 
+
+def one_hot_encoding(df, feature_columns):
+	""" Take the pandas dataframe df and create a one-hot encoding for
+		each of the feature_columns, then drop the actual feature_column
+		as we don't need it anymore. """
+	for col in feature_columns:
+		one_hot = pd.get_dummies(df[col])
+		one_hot.rename(columns = lambda c: "%s_%s" % (col, c.replace(' ','_').lower()), inplace = True)
+		df.drop(col, axis = 1, inplace = True)
+		df =  df.join(one_hot)
+
+	return df
 
 def cleanup():
 	""" Consumes raw data in order to filter our unecessary columns."""
@@ -54,6 +79,7 @@ def cleanup():
 	# trim data and apply feature conversion
 	trimmed_data = data[cols_to_keep]
 	trimmed_data = trimmed_data.applymap(convert_to_feature)
+	trimmed_data = one_hot_encoding(trimmed_data, ['property_type', 'room_type', 'bed_type', 'cancellation_policy'])
 	trimmed_data.to_csv('listings_cleaned.csv', index=False)
 
 if __name__ == '__main__':
